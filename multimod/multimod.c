ba63659b54b712f480440e4f2765e941e685b037
191240004-Chen Bofan <191240004@smail.nju.edu.cn>
2022-10-11 12:56:30 +0800

    pp

--- multimod/multimod.c
+++ multimod/multimod.c
@@ -1,5 +1,38 @@
 #include <stdint.h>
+#include <stdio.h>
+#include <inttypes.h>
 
-uint64_t multimod(uint64_t a, uint64_t b, uint64_t m) {
-  return (a * b) % m; // buggy
+uint64_t mod(uint64_t a, uint64_t m){
+  if(a<m){
+    return a;
+  }else{
+    int i=0; 
+    while(a>=m){
+      uint64_t mm=m;
+      while(a>(mm<<1)){
+        mm<<=1;
+        i++;
+      }
+      a-=mm;
+      mm>>=1;
+    }
+    return a;
+  }
+}
+
+uint64_t addmod(uint64_t a, uint64_t b, uint64_t m){
+  return mod((mod(a,m)+mod(b,m)),m);
+}
+
+
+uint64_t multimod(uint64_t a, uint64_t b, uint64_t m){
+    uint64_t result=0;
+    while(b){
+      if(b&1){
+        result=addmod(result,a,m);
+      }
+      a=addmod(a,a,m);
+      b>>=1;
+    }
+    return result;
 }
