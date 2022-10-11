e1d793671a2029d77c6a1ced0d27b1791b1229c4
191240004-Chen Bofan <191240004@smail.nju.edu.cn>
2022-10-11 12:24:07 +0800

    version1

--- multimod/main.c
+++ multimod/main.c
@@ -4,6 +4,7 @@
 
 uint64_t multimod(uint64_t, uint64_t, uint64_t);
 
+
 void test(uint64_t a, uint64_t b, uint64_t m) {
   #define U64 "%" PRIu64
   printf(U64 " * " U64 " mod " U64 " = " U64 "\n", a, b, m, multimod(a, b, m));
