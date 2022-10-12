af8ae645ccbba0f6ce13fa122a6e4445d061d926
191240004-Chen Bofan <191240004@smail.nju.edu.cn>
2022-10-12 22:38:40 +0800

    lab1

--- multimod/multimod.c
+++ multimod/multimod.c
@@ -1,38 +1,39 @@
+#include <inttypes.h>
 #include <stdint.h>
 #include <stdio.h>
-#include <inttypes.h>
 
-uint64_t mod(uint64_t a, uint64_t m){
-  if(a<m){
+uint64_t mod(uint64_t a, uint64_t m) {
+  if (a < m) {
     return a;
-  }else{
-    int i=0; 
-    while(a>=m){
-      uint64_t mm=m;
-      while(a>(mm<<1)){
-        mm<<=1;
-        i++;
+  } else {
+    while (a >= m) {
+      uint64_t mm = m;
+      while (a > (mm << 1)) {
+        mm <<= 1;
       }
-      a-=mm;
-      mm>>=1;
+      a -= mm;
     }
     return a;
   }
 }
 
-uint64_t addmod(uint64_t a, uint64_t b, uint64_t m){
-  return mod((mod(a,m)+mod(b,m)),m);
+uint64_t addmod(uint64_t a, uint64_t b, uint64_t m) {
+  uint64_t r1 = mod(a, m);
+  uint64_t r2 = mod(b, m);
+  if (r1 >= -r2)
+    return mod(r1 + r2 - m, m);
+  else
+    return mod(r1 + r2, m);
 }
 
-
-uint64_t multimod(uint64_t a, uint64_t b, uint64_t m){
-    uint64_t result=0;
-    while(b){
-      if(b&1){
-        result=addmod(result,a,m);
-      }
-      a=addmod(a,a,m);
-      b>>=1;
+uint64_t multimod(uint64_t a, uint64_t b, uint64_t m) {
+  uint64_t result = 0;
+  while (b) {
+    if (b & 1) {
+      result = addmod(result, a, m);
     }
-    return result;
+    a = addmod(a, a, m);
+    b >>= 1;
+  }
+  return result;
 }
