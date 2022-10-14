76383f860db6029b18357c113d125cac471e63d7
191240004-Chen Bofan <191240004@smail.nju.edu.cn>
2022-10-14 17:32:03 +0800

    version2

--- multimod/main.c
+++ multimod/main.c
@@ -4,6 +4,44 @@
 
 uint64_t multimod(uint64_t, uint64_t, uint64_t);
 
+uint64_t mod(uint64_t a, uint64_t m) {
+  if (a < m) {
+    return a;
+  } else {
+    while (a >= m) {
+      uint64_t mm = m;
+      while (a > (mm << 1)) {
+        mm <<= 1;
+      }
+      a -= mm;
+    }
+    return a;
+  }
+}
+
+uint64_t addmod(uint64_t a, uint64_t b, uint64_t m) {
+  uint64_t r1 = mod(a, m);
+  uint64_t r2 = mod(b, m);
+  if (r2!=0 && r1 >= -r2)
+    return mod(r1 + r2 - m, m);
+  else
+    return mod(r1 + r2, m);
+
+  // return mod(mod(a,m)+mod(b,m),m);
+}
+
+uint64_t multimod(uint64_t a, uint64_t b, uint64_t m) {
+  uint64_t result = 0;
+  while (b) {
+    if (b & 1) {
+      result = addmod(result, a, m);
+    }
+    a = addmod(a, a, m);
+    b >>= 1;
+  }
+  return result;
+}
+
 
 void test(uint64_t a, uint64_t b, uint64_t m) {
   #define U64 "%" PRIu64
--- multimod/multimod.c
+++ multimod/multimod.c
@@ -18,14 +18,14 @@ uint64_t mod(uint64_t a, uint64_t m) {
 }
 
 uint64_t addmod(uint64_t a, uint64_t b, uint64_t m) {
-  // uint64_t r1 = mod(a, m);
-  // uint64_t r2 = mod(b, m);
-  // if (r1 >= -r2)
-  //   return mod(r1 + r2 - m, m);
-  // else
-  //   return mod(r1 + r2, m);
+  uint64_t r1 = mod(a, m);
+  uint64_t r2 = mod(b, m);
+  if (r2!=0 && r1 >= -r2)
+    return mod(r1 + r2 - m, m);
+  else
+    return mod(r1 + r2, m);
 
-  return mod(mod(a,m)+mod(b,m),m);
+  // return mod(mod(a,m)+mod(b,m),m);
 }
 
 uint64_t multimod(uint64_t a, uint64_t b, uint64_t m) {
