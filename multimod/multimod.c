5ed1bc13476f877ed7a357186e9b2e75d85b31d0
191240004-Chen Bofan <191240004@smail.nju.edu.cn>
2022-10-14 17:53:54 +0800

    try

--- multimod/multimod.c
+++ multimod/multimod.c
@@ -6,12 +6,15 @@ uint64_t mod(uint64_t a, uint64_t m) {
   if (a < m) {
     return a;
   } else {
-    while (a >= m) {
-      uint64_t mm = m;
-      while (a > (mm << 1)) {
-        mm <<= 1;
+    int i = 1;
+    uint64_t mm = m;
+    if (a >= mm) {
+      while (a >= (m << 1)) {
+        m <<= 1;
+        i++;
       }
-      a -= mm;
+      a -= m;
+      m >>= 1;
     }
     return a;
   }
@@ -20,7 +23,7 @@ uint64_t mod(uint64_t a, uint64_t m) {
 uint64_t addmod(uint64_t a, uint64_t b, uint64_t m) {
   uint64_t r1 = mod(a, m);
   uint64_t r2 = mod(b, m);
-  if (r2!=0 && r1 >= -r2)
+  if (r2 != 0 && r1 >= -r2)
     return mod(r1 + r2 - m, m);
   else
     return mod(r1 + r2, m);
